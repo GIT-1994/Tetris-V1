cc2f926b98d28e7322ddca68257d375453bf9da8
Vinod Bhume <121920992+GIT-1994@users.noreply.github.com>
2025-11-08 23:47:58 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jackinfranew" # Replace with your actual S3 bucket name
+    bucket = "jackinfranew2" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
